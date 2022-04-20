f7233380a875930f47f3d5f4ffb36e4483160b3b
santi-fr <santi.fr@icloud.com>
2022-04-20 17:43:22 +0200

    Proof-of-Work
    - Proof of Work implemented

--- main.js
+++ main.js
@@ -6,16 +6,27 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
 
     createGenesisBlock() {
@@ -28,7 +39,7 @@ class BlockChain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -42,13 +53,8 @@ class BlockChain {
     }
 }
 let santiCoin = new BlockChain();
+
+console.log("Mining block 1...");
 santiCoin.addBlock(new Block(1, "20/04/2022", {amount: 4}));
+console.log("Mining block 2...");
 santiCoin.addBlock(new Block(2, "20/04/2022", {amount: 10}));
-
-console.log("Is blockchain valid? " + santiCoin.isChainValid());
-
-santiCoin.chain[1].data = {amount : 100};
-santiCoin.chain[1].hash = santiCoin.chain[1].calculateHash();
-
-console.log("Is blockchain valid? " + santiCoin.isChainValid());
-//console.log(JSON.stringify(santiCoin, null, 4));
