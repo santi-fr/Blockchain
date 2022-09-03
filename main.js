d205e1dba2155e81fec3ebb6bc9a92e2231d44f1
santi-fr <santi.fr@icloud.com>
2022-09-03 22:02:32 +0200

    Building app with Angular
    To do: solve "Error: A collection and schematic is required during execution."

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const ec = new EC ("secp256k1");
 const myKey = ec.keyFromPrivate("b665664be3ee4e8810c7b895a49a163ce4f9ca2eeb8f30b79883e9da55712fd3");
 const myWalletAddress = myKey.getPublic('hex');
 
-let santiCoin = new Blockchain();
+let digitalEuro = new Blockchain();
 
 const tx1 = new Transaction(myWalletAddress, "public key goes here", 100);
 
 tx1.signTransaction(myKey);
-santiCoin.addTransaction(tx1);
+digitalEuro.addTransaction(tx1);
 
 console.log("\nStarting the miner...");
-santiCoin.minePendingTransactions(myWalletAddress);
+digitalEuro.minePendingTransactions(myWalletAddress);
 
-console.log("\nBalance of Santi is ", santiCoin.checkBalance(myWalletAddress));
+console.log("\nBalance of Santi is ", digitalEuro.checkBalance(myWalletAddress));
 
-console.log("Is chain valid?", santiCoin.isChainValid());
+console.log("Is chain valid?", digitalEuro.isChainValid());
